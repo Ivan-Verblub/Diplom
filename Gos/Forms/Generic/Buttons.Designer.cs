e781fc65a062cacb39d8af9156ca2c68b7a80534
Ivan <limcmilc@gmail.com>
2022-04-11 11:23:05 +0300

    Для продолжения

--- Gos/Forms/Generic/Buttons.Designer.cs
+++ Gos/Forms/Generic/Buttons.Designer.cs
@@ -70,7 +70,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 112);
+            this.ClientSize = new System.Drawing.Size(800, 406);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
